3b623f641b681c3a398a9a6b15c43e99bc555b46
Amir Negative <negative_amir@yahoo.com>
2016-02-22 13:59:40 +0330

    Added files via upload

---
+++ plugins/SUDO.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return [[
+  sudo user :
+  @negative_officiall
+  
+  ]]
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "!sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^/sudoers$",
+    "^!sudoers$",
+    "^sudoers$"
+  },
+  run = run
+}
+end
